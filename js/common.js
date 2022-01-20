14f37e14202c749e0abd275fca75f87e079699fb
Apfelsinen <31432411+Apfelsinen@users.noreply.github.com>
2022-01-20 21:24:00 +0800

    com

--- js/common.js
+++ js/common.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	// carusel section
-
+	const showSlider = new Swiper('.showcase__carousel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1800
+	})
+	//video
 	document.querySelector('video').playbackRate = 2
 
 })
